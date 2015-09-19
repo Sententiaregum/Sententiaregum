a65f53a6a65ac697dfeb85c018fb5af7909dab52
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2015-09-19 13:54:14 -0400

    Applied fixes from StyleCI

--- web/app.php
+++ web/app.php
@@ -3,13 +3,13 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
 
-require_once __DIR__ . '/../app/AppKernel.php';
+require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
