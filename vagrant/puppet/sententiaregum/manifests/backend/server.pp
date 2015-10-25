18fb1df11fc161923d5a3048c2ea0891be176b65
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2015-10-25 21:12:02 +0100

    Refactor #43 improved sententiaregum puppet module
    - speedup provisioning through avoid compilation of the nodejs package
    - make server parameters configurable
    - let hiera parse another "local.yaml" file that is able to override all parameters of the common.yaml
    - resolves #43

--- vagrant/puppet/sententiaregum/manifests/backend/server.pp
+++ vagrant/puppet/sententiaregum/manifests/backend/server.pp
@@ -1,14 +1,26 @@
-class sententiaregum::backend::server($host_name, $doc_root) {
+class sententiaregum::backend::server(
+  $host_name,
+  $doc_root,
+  $front_controller = 'app_dev.php',
+  $server_aliases   = '',
+  $port             = 80,
+  $modules          = {}
+) {
   validate_string($host_name)
   validate_string($doc_root)
+  validate_string($front_controller)
+  validate_string($server_aliases)
+  validate_integer($port)
+  validate_hash($modules)
 
-  class { 'apache': }
+  class { '::apache': }
 
-  apache::module { 'rewrite': }
+  create_resources('::apache::module', $modules)
+
   apache::vhost { $host_name:
-    server_name              => $host_name,
-    port                     => 80,
-    docroot                  => $doc_root,
-    directory_allow_override => 'All',
+    server_name   => $host_name,
+    port          => $port,
+    docroot       => $doc_root,
+    serveraliases => $server_aliases,
   }
 }
