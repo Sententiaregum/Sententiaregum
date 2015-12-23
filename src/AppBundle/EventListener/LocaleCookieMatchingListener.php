39bee1cdaa53f367f633a316b14c33e9cbd3a828
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2015-12-23 11:48:48 +0100

    Blocker #144 remove lunetics/locale and create custom matcher

---
+++ src/AppBundle/EventListener/LocaleCookieMatchingListener.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Sententiaregum project.
+ *
+ * (c) Maximilian Bosch <maximilian.bosch.27@gmail.com>
+ * (c) Ben Bieler <benjaminbieler2014@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Listener which configures the internal language according to the content of a specific cookie.
+ *
+ * @author Maximilian Bosch <maximilian.bosch.27@gmail.com>
+ */
+class LocaleCookieMatchingListener
+{
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+
+    /**
+     * Constructor.
+     *
+     * @param string $defaultLocale
+     */
+    public function __construct($defaultLocale)
+    {
+        $this->defaultLocale = (string) $defaultLocale;
+    }
+
+    /**
+     * Hook which detects the locale.
+     *
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST === $event->getRequestType()) {
+            $request = $event->getRequest();
+            $locale  = $request->cookies->has('language') ? $request->cookies->get('language') : $this->defaultLocale;
+
+            if ($locale !== $request->getLocale()) {
+                $request->setLocale($locale);
+            }
+
+            $localeAttribute = '_locale';
+            if ($request->attributes->get($localeAttribute) !== $locale) {
+                $request->attributes->set($localeAttribute, $locale);
+            }
+        }
+    }
+}
---
+++ src/AppBundle/Tests/EventListener/LocaleCookieMatchingListenerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Sententiaregum project.
+ *
+ * (c) Maximilian Bosch <maximilian.bosch.27@gmail.com>
+ * (c) Ben Bieler <benjaminbieler2014@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Tests\EventListener;
+
+use AppBundle\EventListener\LocaleCookieMatchingListener;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class LocaleCookieMatchingListenerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMatchLocale()
+    {
+        $request = Request::create('/');
+        $request->cookies->set('language', 'de');
+
+        $event    = new GetResponseEvent($this->getMock(HttpKernelInterface::class), $request, HttpKernelInterface::MASTER_REQUEST);
+        $listener = new LocaleCookieMatchingListener('en');
+
+        $listener->onKernelRequest($event);
+
+        $this->assertSame('de', $request->getLocale());
+        $this->assertSame('de', $request->attributes->get('_locale'));
+    }
+
+    public function testSubRequest()
+    {
+        $request = Request::create('/');
+        $request->cookies->set('language', 'de');
+        $request->setLocale('en');
+        $request->attributes->set('_locale', 'en');
+
+        $event    = new GetResponseEvent($this->getMock(HttpKernelInterface::class), $request, HttpKernelInterface::SUB_REQUEST);
+        $listener = new LocaleCookieMatchingListener('en');
+
+        $listener->onKernelRequest($event);
+
+        $this->assertSame('en', $request->getLocale());
+        $this->assertSame('en', $request->attributes->get('_locale'));
+    }
+}
